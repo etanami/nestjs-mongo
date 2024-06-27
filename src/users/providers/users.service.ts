d27fdfccc8acd04986b61ab90e88e3eb99407a59
Manik <manik@pixelconception.com>
2024-06-27 14:48:51 +0530

    Final Code For NSJS405

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,10 +1,30 @@
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { Injectable } from '@nestjs/common';
+import { User } from '../user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from '../dtos/create-user.dto';
 /**
  * Class to connect to Users table and perform business operations
  */
 @Injectable()
 export class UsersService {
+  constructor(
+    /**
+     * Inject UserModel
+     */
+    @InjectModel(User.name)
+    private readonly userModel: Model<User>,
+  ) {}
+
+  /**
+   * Method to create a new user
+   */
+  public async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = new this.userModel(createUserDto);
+    return await newUser.save();
+  }
+
   /**
    * The method to get all the users from the database
    */
