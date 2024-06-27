0436647e15f61555889ff2ed9e0e15d03bb59561
Manik <manik@pixelconception.com>
2024-06-27 14:06:38 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://nestjs:4kRbli2h2k5F50tb@nestjs.dgq4bvd.mongodb.net/?retryWrites=true&w=majority&appName=nestjs',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
