3af2212e03e67e4aa8bd5802781c1536b3492560
Manik <manik@pixelconception.com>
2024-06-27 16:38:19 +0530

    Final Code For NSJS411

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { TagsService } from './providers/tags.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateTagDto } from './dtos/create-tag.dto';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tagsService: TagsService) {}
+  @Post()
+  public createTag(@Body() createTagDto: CreateTagDto) {
+    return this.tagsService.createTag(createTagDto);
+  }
+}
