74d22a35819864a9bc38a71046b5c6a1f196e921
Manik <manik@pixelconception.com>
2024-06-27 16:30:58 +0530

    Final Code For NSJS409

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
+import { TagsModule } from './tags/tags.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from './users/users.module';
       'mongodb+srv://nestjs:4kRbli2h2k5F50tb@nestjs.dgq4bvd.mongodb.net/?retryWrites=true&w=majority&appName=nestjs',
       { dbName: 'nestjs-blog' },
     ),
+    TagsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
